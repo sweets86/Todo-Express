5b530cf2bafa93f74e9821d108b1e8087fa2ebbd
yanica svensson <sweets86_@hotmail.com>
2020-06-23 20:10:41 +0200

    adding already created

---
+++ public/logic.js
@@ -0,0 +1,81 @@
+
+
+window.onload = main
+
+function main() {
+    const div = document.querySelector('div')
+
+    const formDiv = document.createElement('div')
+    formDiv.classList = "form"
+
+    const input = document.createElement('input')
+
+    const buttonAdd = document.createElement('button')
+    buttonAdd.classList = "button"
+    buttonAdd.innerHTML = "Lägg till Todo"
+   /*  button.onclick = function () {
+        addNewTodo()
+    } */
+
+    const button = document.createElement('button')
+    button.classList = "button"
+    button.innerHTML = "Lista alla Todo"
+    button.onclick = function () {
+        getAllTodos()
+    }
+
+    div.append(formDiv)
+    formDiv.append(input)
+    formDiv.append(buttonAdd)
+    formDiv.append(button)
+
+}
+
+async function getAllTodos() {
+    const allTodos = await makeRequest('/todos', 'GET')
+    console.log(allTodos)
+
+    const div = document.querySelector('div')
+    const listDiv = document.createElement('div')
+    listDiv.classList = "form"
+
+    for (let i = 0; i < allTodos.length; i++) {
+        let todos = allTodos[i]
+
+        const ul = document.createElement('ul')
+
+        const li = document.createElement('li')
+        li.innerText = todos
+        console.log(todos)
+
+        const removeButton = document.createElement('button')
+        removeButton.classList = "button"
+        removeButton.innerHTML = "Ta bort Todo"
+
+        const viewButton = document.createElement('button')
+        viewButton.classList = "button"
+        viewButton.innerHTML = "Se Todo"
+
+        listDiv.append(ul)
+        ul.append(li)
+        li.append(removeButton)
+        li.append(viewButton)
+        div.append(listDiv)
+    }
+}
+
+
+
+
+async function makeRequest(url, reqMethod, body) {
+
+    const response = await fetch(url, {
+        headers: { "Content-Type": "application/json" },
+        method: reqMethod,
+        body: JSON.stringify(body)
+    })
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    return data // return skickar med data så den kan användas vidare
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+let todos = [
+    "städa", "handla", "träna", "laga mat"
+]
+
+app.get('/todos', (req, res) => {
+    res.json(todos)
+})
+
+app.get('/todos/:id', (req, res) => {
+    res.json("En specifik todo")
+})
+
+app.post('/todos', (req, res) => {
+    res.json("Ny todo")
+})
+
+app.put('/todos/:id', (req, res) => {
+    res.json("Uppdaterad todo")
+})
+
+app.delete('/todos/:id', (req, res) => {
+    res.json("Borttagen todo")
+})
+
+
+
+
+
+
+app.listen(3000, 'localhost', () => {
+    console.log("Server at port 3000")
+})
